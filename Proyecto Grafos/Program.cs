2eaf426f789f0799b72ed68e2223400a35125d4b
Isaac Arias <ariaspicado2007@gmail.com>
2026-03-25 23:24:38 -0600

    cambios de perfilado

---
+++ Proyecto Grafos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+using Proyecto_Grafos.UI.Forms;
+
+namespace Proyecto_Grafos
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
+    
